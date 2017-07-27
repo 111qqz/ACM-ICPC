71f2a4559dda4a88f266917af9e125c09e7409bc
renkuanze <renkuanze@sensetime.com>
2017-07-27 20:48:04 +0800

    test

---
+++ b.cpp
@@ -0,0 +1,44 @@
+/* ***********************************************
+Author :111qqz
+Created Time :2017年07月27日 星期四 20时47分01秒
+File Name :b.cpp
+************************************************ */
+
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#define PB push_back
+#define fst first
+#define sec second
+#define lson l,m,rt<<1
+#define rson m+1,r,rt<<1|1
+#define ms(a,x) memset(a,x,sizeof(a))
+typedef long long LL;
+#define pi pair < int ,int >
+#define MP make_pair
+
+using namespace std;
+const double eps = 1E-8;
+const int dx4[4]={1,0,0,-1};
+const int dy4[4]={0,-1,1,0};
+const int inf = 0x3f3f3f3f;
+int main()
+{
+        #ifndef  ONLINE_JUDGE 
+        freopen("./in.txt","r",stdin);
+  #endif
+    puts("test ignore");
+  #ifndef ONLINE_JUDGE  
+  fclose(stdin);
+  #endif
+    return 0;
+}
